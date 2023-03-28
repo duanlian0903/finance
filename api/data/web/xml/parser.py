1c7cf61e9c7edd009a364f666697605b52a8c3b1
lianduan <duanlian.cn@gmail.com>
2023-03-28 15:53:26 -0400

    c

--- api/data/web/xml/parser.py
+++ api/data/web/xml/parser.py
@@ -6,10 +6,18 @@ def get_soup_structure(html_source_code):
     return bs4.BeautifulSoup(html_source_code, 'html.parser')
 
 
+def get_all_tags(soup_structure, tag_text):
+    return soup_structure.find_all(tag_text)
+
+
+def get_tag_attribute(tag, attribute_text):
+    return tag.get(attribute_text)
+
+
 def get_all_the_urls_in_the_given_webpage(given_webpage_url):
     html_source_code = adwxsc.get_xml_source_code(given_webpage_url)
     soup_structure = get_soup_structure(html_source_code)
     urls = []
-    for link in soup_structure.find_all('a'):
-        urls.append(link.get('href'))
+    for tag in get_all_tags(soup_structure, 'a'):
+        urls.append(get_tag_attribute(tag, 'href'))
     return urls
--- api/data/web/xml/source_code.py
+++ api/data/web/xml/source_code.py
@@ -1,6 +1,10 @@
 import requests
 
 
+def get_requests_header():
+    return {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+
 def get_xml_source_code(url):
-    req = requests.get(url)
+    req = requests.get(url, headers=get_requests_header())
     return req.text
