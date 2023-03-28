78b9f9397d3b976134ed6cec6abbd79667f3e451
lianduan <duanlian.cn@gmail.com>
2023-03-28 17:57:08 -0400

    continue

--- api/data/web/xml/parser.py
+++ api/data/web/xml/parser.py
@@ -1,5 +1,6 @@
 import bs4
 import api.data.web.xml.source_code as adwxsc
+import api.common.data_type_operation.check_data_type as acdtocdt
 
 
 def get_soup_structure(html_source_code):
@@ -10,11 +11,17 @@ def get_soup_structure(html_source_code):
 
 
 def get_all_tags(soup_structure, tag_text):
-    return soup_structure.find_all(tag_text)
+    try:
+        return soup_structure.find_all(tag_text)
+    except:
+        return None
 
 
 def get_tag_attribute(tag, attribute_text):
-    return tag.get(attribute_text)
+    try:
+        return tag.get(attribute_text)
+    except:
+        None
 
 
 def get_all_the_urls_in_the_given_webpage(given_webpage_url):
@@ -22,5 +29,7 @@ def get_all_the_urls_in_the_given_webpage(given_webpage_url):
     soup_structure = get_soup_structure(html_source_code)
     urls = []
     for tag in get_all_tags(soup_structure, 'a'):
-        urls.append(get_tag_attribute(tag, 'href'))
+        href_attribute = get_tag_attribute(tag, 'href')
+        if acdtocdt.whether_string(href_attribute):
+            urls.append(href_attribute)
     return urls
