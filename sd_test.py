f0983114be86e102256f014a9f187557bba77dc2
Lian Duan <duanlian.cn@gmail.com>
2023-04-25 18:03:14 -0400

    test stable diffusion model

--- sd_test.py
+++ sd_test.py
@@ -1,5 +1,6 @@
 import keras_cv
 import matplotlib.pyplot as plt
+import tkinter as tk
 
 
 def create_images(text_description, img_width=512, img_height=512, batch_size=3):
@@ -17,4 +18,26 @@ def plot_images(images):
     plt.show()
 
 
-plot_images(create_images("one dog chase two cats, fantasy art, golden color"))
+def show_image():
+    plot_images(create_images(tb_description.get(), img_width=int(tb_width.get()), img_height=int(tb_height.get()), batch_size=int(tb_batch_size.get())))
+
+
+master = tk.Tk()
+tk.Label(master, text='Description').grid(row=0)
+tb_description = tk.Entry(master, width=50)
+tb_description.insert(0, 'dog drink milk, fancy, cartoon')
+tb_description.grid(row=0, column=1)
+tk.Label(master, text='Width').grid(row=1)
+tb_width = tk.Entry(master, width=50)
+tb_width.insert(0, '512')
+tb_width.grid(row=1, column=1)
+tk.Label(master, text='Height').grid(row=2)
+tb_height = tk.Entry(master, width=50)
+tb_height.insert(0, '512')
+tb_height.grid(row=2, column=1)
+tk.Label(master, text='Sample Number').grid(row=3)
+tb_batch_size = tk.Entry(master, width=50)
+tb_batch_size.insert(0, '3')
+tb_batch_size.grid(row=3, column=1)
+tk.Button(master, text='Submit', command=show_image).grid(row=4, column=1)
+master.mainloop()
